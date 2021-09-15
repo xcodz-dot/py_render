f753dec3e0dfb9a4714411667fabe35c16981e84
xcodz-dot <71920621+xcodz-dot@users.noreply.github.com>
2021-09-15 16:09:41 +0530

    :sparkles: Add BoxBlur Layer

--- py_render/layers/blur.py
+++ py_render/layers/blur.py
@@ -6,8 +6,17 @@ __author__ = "xcodz-dot"
 
 class Blur(Layer):
     def __init__(self, blur: float):
-        super().__init__("Blur", "Blurs a image based on blur density")
+        super().__init__("Blur", "Blurs an image based on blur density")
         self._filter = ImageFilter.GaussianBlur(blur)
 
+    def apply(self, im: Image.Image) -> Image.Image:
+        return im.filter(self._filter)
+
+
+class BoxBlur(Layer):
+    def __init__(self, blur: float):
+        super().__init__("BoxBlur", "Blurs an image based on blur density using average color per box method")
+        self._filter = ImageFilter.BoxBlur(blur)
+    
     def apply(self, im: Image.Image) -> Image.Image:
         return im.filter(self._filter)
